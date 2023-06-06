07cd0f26576780ec4e1de31cd6c66361108c03ad
john665paes <johnatanpaes@gmail.com>
2023-06-06 18:05:36 -0300

    tentando fazer o inimigo

--- Assets/Scripts/Inimigos/Inimigo.cs
+++ Assets/Scripts/Inimigos/Inimigo.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inimigo : MonoBehavior {
+public class Inimigo : MonoBehaviour {
 
     private GameObject player;
     public float velocidade = 2f;
+    private bool podeMover = true;
+    private Animator animator;
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
     void Start(){
         player = GameObject.FindGameObjectWithTag("Player");
 
     }
-     void update(){
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, velocidade*Time.deltatime);
+    void update(){    
+
+        animator.SetBool("podeMover", podeMover);
+        if (podeMover)
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, velocidade * Time.deltaTime);
+    }
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") podeMover = false;
+    }
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") podeMover = true;
     }
 
 }
